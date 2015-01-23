8bf1f73e6f70fd291f32721c00ca34def6b9d2b7
congyuandong <congyuandong@gmail.com>
2015-01-23 16:58:29 +0800

    修改短信接口密码

--- qdvote/tools.py
+++ qdvote/tools.py
@@ -23,7 +23,7 @@ def SendRandomCode(tel):
 	url = 'http://www.mxtong.net.cn/GateWay/Services.asmx/DirectSend'
 	code = RandCode()
 	content = '您的验证码为'+code+'。工作人员不会向您索要，请勿向任何人泄漏。【清大益讯】'
-	payload = {'UserID':'965125','Account':'admin','Password':'DUWBT2','Phones':tel,
+	payload = {'UserID':'965125','Account':'admin','Password':'965125','Phones':tel,
 				'Content':content,'SendTime':'','SendType':'1','PostFixNumber':''}
 	r = requests.get(url,params=payload)
 	#xml = ElementTree.fromstring(r.content)
